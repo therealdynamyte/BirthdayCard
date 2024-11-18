efe29a5627776bb889ac9c42c13cfda4baa991a7
Dynamyte <haryordhayor@gmail.com>
2024-11-18 18:16:28 +0100

    Add files via upload

---
+++ birthday.js
@@ -0,0 +1,20 @@
+const bodyElement = document.querySelector("body");
+
+
+bodyElement.addEventListener("mousemove", (event) => {
+    //console.log("moved")
+
+    const xPos = event.offsetX;
+    // console.log(xPos);
+    const yPos = event.offsetY;
+     const spanElement = document.createElement("span");
+    spanElement.style.left = xPos + "px";
+    spanElement.style.top = yPos + "px";
+    const size = Math.random() * 100;
+    spanElement.style.width = size + "px";
+    spanElement.style.height = size + "px";
+    bodyElement.appendChild(spanElement);
+    setTimeout(()=> {
+        spanElement.remove();
+    }, 2000);
+})
